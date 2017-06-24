dcf8fc918d3c4ff617eddd56049535cbcf1eb77c
Tomasz Maćkowiak <kurazu@kurazu.net>
2017-06-24 18:35:55 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  // parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'script'
+  },
+  env: {
+    es6: true,
+    node: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': 0,
+    'no-var': 2
+  }
+}
